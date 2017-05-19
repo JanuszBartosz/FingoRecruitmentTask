2a6304fdb0fe99dc1017d21cd1bf46778c23545c
Bartosz Janusz <janusz.bartek@gmail.com>
2017-05-19 13:56:13 +0200

    Init commit

---
+++ src/main/java/fingo/recruitment/task/FingoRecruitmentTaskApplication.java
@@ -0,0 +1,12 @@
+package fingo.recruitment.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingoRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingoRecruitmentTaskApplication.class, args);
+	}
+}
